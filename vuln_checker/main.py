cb6e4f9424771c9d98462eaebf94175ace6b335c
Nikita Pivkin <100182843+nikpivkin@users.noreply.github.com>
2023-02-21 16:14:19 +0600

    Prepare databases (#4)
    * feat: prepare databases
    
    Clone the main repository `vuln-list` with databases. Clone the `gitlab advisory-database` repository and use it instead of the `glad` database in vuln-list.
    
    * refactor: update gitlab loader
    
    * build: fix poetry.lock
    
    * fix: aliases must be a set
    
    * refactor: elegantly change the extension
    
    * refactor: use `list` as the default value for aliases

--- vuln_checker/main.py
+++ vuln_checker/main.py
@@ -1,16 +1,23 @@
 import itertools
 import json
 import logging
+import os
+import shutil
 from pathlib import Path
 from typing import Iterable, TypedDict
 
 import networkx as nx
+import yaml
+from git import Repo, RemoteProgress
+from tqdm import tqdm
 
 from vuln_checker.load import load_vulnerabilities
 
 DEFAULT_DATABASES_PATH = Path.cwd() / "vuln-list"
 VULNS_PATH = Path.cwd() / "vulnerabilities"
 
+VULN_LIST_REPO = "https://github.com/aquasecurity/vuln-list.git"
+GLAD_DATABASE_REPO = "https://gitlab.com/gitlab-org/security-products/gemnasium-db.git"
 
 def check_databases_folder(databases_path: Path):
     if not databases_path.exists():
@@ -21,6 +28,64 @@ def set_to_list(obj):
     if isinstance(obj, set):
         return list(obj)
     raise TypeError
+
+
+class Progress(RemoteProgress):
+    def __init__(self):
+        super().__init__()
+        self.pbar = tqdm()
+
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        self.pbar.total = max_count
+        self.pbar.n = cur_count
+        self.pbar.refresh()
+
+
+def prepare_databases():
+    if os.path.exists(DEFAULT_DATABASES_PATH):
+        logging.info("The databases are already loaded")
+        return
+
+    logging.info("Clone `vuln_list` repo")
+    Repo.clone_from(
+        VULN_LIST_REPO,
+        DEFAULT_DATABASES_PATH,
+        progress=Progress(),
+        single_branch=True,
+        depth=1
+    )
+
+    glad_database_path = os.path.join(DEFAULT_DATABASES_PATH, "glad")
+
+    logging.info("Remove `glad` database from `vuln_list`")
+    if os.path.exists(glad_database_path):
+        shutil.rmtree(glad_database_path)
+
+    logging.info("Clone `GitLab Advisory Database` repo")
+    Repo.clone_from(
+        GLAD_DATABASE_REPO,
+        glad_database_path,
+        progress=Progress(),
+        single_branch=True,
+        depth=1
+    )
+
+    logging.info("Convert files .yml in .json in the `glad` database")
+    glad_path = Path(glad_database_path)
+    packages = ("conan", "gem", "go", "maven", "npm", "nuget", "packagist", "pypi")
+    for directory in glad_path.iterdir():
+        if directory.name not in packages:
+            if directory.is_dir():
+                shutil.rmtree(directory.absolute())
+            else:
+                directory.unlink()
+            continue
+        for p in tqdm((glad_path / directory).rglob("*.yml"), desc=f"Convert `{directory.name}` package"):
+            json_advisory = p.with_suffix(".json")
+            with p.open("r") as yaml_file, json_advisory.open("w") as json_file:
+                advisory = yaml.safe_load(yaml_file)
+                json.dump(advisory, json_file)
+            p.unlink()
 
 
 priorities = frozenset([
@@ -110,7 +175,7 @@ def combine_vulnerabilities(vulns: VulnMap) -> VulnMap:
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    check_databases_folder(DEFAULT_DATABASES_PATH)
+    prepare_databases()
     VULNS_PATH.mkdir(exist_ok=True)
 
     vulnerabilities = create_map_of_vulnerabilities()
