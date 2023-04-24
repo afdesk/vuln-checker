de1cc1e761db3be4b48183510a373d9d2c62061d
Nikita Pivkin <nikita.pivkin@smartforce.io>
2023-04-24 11:33:42 +0600

    move test

--- tests/graphql_test.py
+++ tests/graphql_test.py
@@ -1,8 +1,11 @@
+import pytest
+
 from vuln_checker.gh.graphql import update_discussion, get_discussion
 
 TEST_DISCUSSION_ID = "D_kwDOI_AQL84ATegE"
 
 
+@pytest.mark.skip
 def test_update_discussion():
     title = "test title"
     discussion = update_discussion(discussion_id=TEST_DISCUSSION_ID, title=title)
@@ -11,6 +14,7 @@ def test_update_discussion():
     assert discussion["title"] == title
 
 
+@pytest.mark.skip
 def test_get_discussion():
     discussion = get_discussion(discussion_id=TEST_DISCUSSION_ID)
     assert discussion["id"] == TEST_DISCUSSION_ID
